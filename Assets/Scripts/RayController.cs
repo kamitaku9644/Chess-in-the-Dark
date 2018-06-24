5b23e6c3530c947e150e96ce96c69cb246db44f3
kamitaku <kamitaku44@gmail.com>
2018-06-25 00:29:24 +0900

    raycontroller 実装

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RayController : SelectGameManager
+public class RayController : GameManager
 {
+    
+    enum SelectState { playerSelect, squareSelect };
+    SelectState selectState;
+    public GameObject mainCamera;
 
-    public GameObject hittedObj;
+    bool rayhitted;
+    GameObject hittedObj;
 
     GameObject hittedPlayer;
 
@@ -16,9 +21,165 @@ public class RayController : SelectGameManager
     GameObject hittedSquare;
     ILoading squareLoad;
 
+    void Start()
+    {
+        selectState = SelectState.playerSelect;
+        rayhitted = false;
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            hittedObj = hit.collider.gameObject;
+            rayhitted = true;
+
+            switch (selectState)
+            {
+                case SelectState.playerSelect:
+
+                    if (hittedObj.tag == "Player1")
+                    {
+                        if (hittedPlayer == null)
+                        {
+                            hittedPlayer = hittedObj;
+                            playerLoad = hittedPlayer.GetComponentInChildren<ILoading>();
+                            hittedPlayer.GetComponent<IMovable>().Movable();
+                        }
+                        else
+                        {
+                            playerLoad.Loading();
+                            if (playerLoad.LoadComp()) { selectState = SelectState.squareSelect; }
+
+                        }
+                    }
+                                      
+                    break;
 
 
-    public bool RayHittd()
+
+                case SelectState.squareSelect:
+
+                    //playseselect cancel
+                    if (hittedObj.tag == "Player1")
+                    {
+                        if (hesHittedPlayer == null)
+                        {
+                            hesHittedPlayer = hittedObj;
+                            playerLoad = hesHittedPlayer.GetComponentInChildren<ILoading>();
+                        }
+                        else
+                        {
+                            playerLoad.Loading();
+
+                            if (playerLoad.LoadComp())
+                            {
+                                hittedPlayer.GetComponent<IMovable>().SSinit();
+                                hittedPlayer = null;
+                                selectState = SelectState.playerSelect;
+                            }
+
+                        }
+                    }
+
+
+                    //square select
+                    if (hittedObj.tag == "SelectableSquare")
+                    {
+                        if (hittedSquare == null)
+                        {
+                            hittedSquare = hittedObj;
+                            squareLoad = hittedSquare.GetComponentInChildren<ILoading>();
+                        }
+                        else
+                        {
+                            squareLoad.Loading();
+                            if (squareLoad.LoadComp())
+                            {
+                                hittedPlayer.GetComponentInChildren<IMove>().Move(hittedSquare);
+                                //call move
+                                print("called move");
+                            }
+                        }
+                    }
+
+
+                    break;
+            }
+
+        }
+
+        //initiallization
+        else if(rayhitted){
+            hittedObj = null;
+            rayhitted = false;
+
+
+            switch (selectState)
+            {
+                case SelectState.playerSelect:
+
+                    
+
+                    if (hittedPlayer != null)
+                    {
+                        playerLoad.Loadinit();
+                        hittedPlayer.GetComponent<IMovable>().SSinit();
+                        hittedPlayer = null;
+                    }
+
+                    break;
+
+                case SelectState.squareSelect:
+
+                    //playseselect cancel
+                    if (hesHittedPlayer != null)
+                    {
+                        playerLoad.Loadinit();
+                        hesHittedPlayer = null;
+                    }
+
+
+
+                    //square select
+                    if (hittedSquare != null)
+                    {
+                        squareLoad.Loadinit();
+                        hittedSquare = null;
+                    }
+
+
+                    break;
+            }
+        }
+
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    public void RayHitted()
     {
         Ray ray = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
         RaycastHit hit;
@@ -26,9 +187,9 @@ public class RayController : SelectGameManager
         if (Physics.Raycast(ray, out hit))
         {
             hittedObj = hit.collider.gameObject;
-            return true;
+            
         }
-        return false;
+        return;
     }
 
 
@@ -122,7 +283,7 @@ public class RayController : SelectGameManager
             }
         }
     }
-
+    */
 
 
 
