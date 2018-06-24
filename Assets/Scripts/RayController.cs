9541eb5eb0772b5f6ca44f8ba547aed5437bc8bb
kamitaku <kamitaku44@gmail.com>
2018-06-24 19:42:37 +0900

    timemanager raycontroller(rayhitted..) selectgamemanager実装　error:stackoverflow

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RayController : MonoBehaviour
+public class RayController : SelectGameManager
 {
 
-    public GameObject MainCamera;
+    public GameObject hittedObj;
 
     GameObject hittedPlayer;
 
@@ -16,21 +16,125 @@ public class RayController : MonoBehaviour
     GameObject hittedSquare;
     ILoading squareLoad;
 
-    bool playerSelected;
-    bool squareSelected;
 
-    // Use this for initialization
-    void Start()
+
+    public bool RayHittd()
     {
-        playerSelected = false;
-        squareSelected = false;
+        Ray ray = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            hittedObj = hit.collider.gameObject;
+            return true;
+        }
+        return false;
     }
+
+
+    public void PlayerSelect()
+    {
+        if (hittedObj.tag == "Player1")
+        {
+            if (hittedPlayer == null)
+            {
+                hittedPlayer = hittedObj;
+                playerLoad = hittedPlayer.GetComponentInChildren<ILoading>();
+                hittedPlayer.GetComponent<IMovable>().Movable();
+            }
+            else
+            {
+                playerLoad.Loading();
+                if (playerLoad.LoadComp()) { playerSelected = true; }
+
+            }
+        }
+        else if (hittedPlayer != null)
+        {
+            playerLoad.Loadinit();
+            hittedPlayer.GetComponent<IMovable>().SSinit();
+            hittedPlayer = null;
+        }
+
+    }
+
+
+    public void PlayerSelectCancel()
+    {
+        if (hittedObj.tag == "Player1")
+        {
+            if (hesHittedPlayer == null)
+            {
+                hesHittedPlayer = hittedObj;
+                playerLoad = hesHittedPlayer.GetComponentInChildren<ILoading>();
+            }
+            else
+            {
+                playerLoad.Loading();
+                if (playerLoad.LoadComp())
+                {
+                    hittedPlayer.GetComponent<IMovable>().SSinit();
+                    hittedPlayer = null;
+                    playerSelected = false;
+                }
+
+            }
+        }
+        else
+        {
+            if (hesHittedPlayer != null)
+            {
+                playerLoad.Loadinit();
+                hesHittedPlayer = null;
+            }
+        }
+    }
+
+
+
+    public void SquareSelect()
+    {
+        if (hittedObj.tag == "SelectableSquare")
+        {
+            if (hittedSquare == null)
+            {
+                hittedSquare = hittedObj;
+                squareLoad = hittedSquare.GetComponentInChildren<ILoading>();
+            }
+            else
+            {
+                squareLoad.Loading();
+                if (squareLoad.LoadComp())
+                {
+                    hittedPlayer.GetComponentInChildren<IMove>().Move(hittedSquare);
+                    //call move
+                    print("called move");
+                }
+            }
+
+        }
+        else
+        {
+            if (hittedSquare != null)
+            {
+                squareLoad.Loadinit();
+                hittedSquare = null;
+            }
+        }
+    }
+
+
+
+
+
+
+/*
+    
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
-        RaycastHit hit;
+        
 
 
         //(state)player select
@@ -126,5 +230,5 @@ public class RayController : MonoBehaviour
                 }
             }
         }
-    }
+    }*/
 }
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : SelectGameManager {
+
+    public float limitTime;
+
+    public bool TimeCount()
+    {
+        limitTime -= Time.deltaTime;
+        if(limitTime >= 0) { return true; }
+        return false;
+    }
+	
+}
